99095b9b593c004f433ecf7d8916ec475f5e30ee
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-03 09:44:03 -0300

    docs(updated_at): atualizado os maps de exemplo no arquivo

--- 04-lists/02-map.js
+++ 04-lists/02-map.js
@@ -3,17 +3,20 @@ const service = require('./service');
 async function main() {
   try {
     
-    // const results = await service.obterPessoas('a')
+    const results = await service.obterPessoas('a')
     // const names = []
     
     // results.results.forEach(function(item) {
     //   names.push(item.name)
     // })
 
-    const names = results.results.map(function(pessoa) {
-      return pessoa.name
-    })
+    // const names = results.results.map(function(pessoa) {
+    //   return pessoa.name
+    // })
 
+    const names = results.results.map((pessoa) => pessoa.name)
+
+    console.log('names', names)
   } catch (error) {
     console.log('Deu Ruim ', error)
   }
